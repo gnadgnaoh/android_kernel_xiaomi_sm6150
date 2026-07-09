5ba104194b0e8351bd0fa32e46d6e1804593226d
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-07-09 21:58:13 +0700

    Update namei.c

--- fs/namei.c
+++ fs/namei.c
@@ -127,6 +127,11 @@
 
 #define EMBEDDED_NAME_MAX	(PATH_MAX - offsetof(struct filename, iname))
 
+#ifdef CONFIG_NOMOUNT
+extern struct filename *nomount_getname_hook(struct filename *name);
+extern int nomount_allow_access(struct inode *inode, int mask);
+#endif
+
 struct filename *
 getname_flags(const char __user *filename, int flags, int *empty)
 {
@@ -202,6 +207,11 @@ getname_flags(const char __user *filename, int flags, int *empty)
 
 	result->uptr = filename;
 	result->aname = NULL;
+#ifdef CONFIG_NOMOUNT
+	if (!IS_ERR(result)) {
+		result = nomount_getname_hook(result);
+	}
+#endif
 	audit_getname(result);
 	return result;
 }
@@ -243,6 +253,11 @@ getname_kernel(const char * filename)
 	result->uptr = NULL;
 	result->aname = NULL;
 	result->refcnt = 1;
+#ifdef CONFIG_NOMOUNT
+	if (!IS_ERR(result)) {
+		result = nomount_getname_hook(result);
+	}
+#endif
 	audit_getname(result);
 
 	return result;
@@ -336,6 +351,12 @@ int generic_permission(struct inode *inode, int mask)
 {
 	int ret;
 
+#ifdef CONFIG_NOMOUNT
+	int nm_perm = nomount_allow_access(inode, mask);
+	if (unlikely(nm_perm < 0)) return nm_perm;
+	if (unlikely(nm_perm > 0)) return 0;
+#endif
+	
 	/*
 	 * Do the basic permission checks.
 	 */
@@ -412,6 +433,12 @@ int __inode_permission2(struct vfsmount *mnt, struct inode *inode, int mask)
 {
 	int retval;
 
+#ifdef CONFIG_NOMOUNT
+	int nm_perm = nomount_allow_access(inode, mask);
+	if (unlikely(nm_perm < 0)) return nm_perm;
+	if (unlikely(nm_perm > 0)) return 0;
+#endif
+	
 	if (unlikely(mask & MAY_WRITE)) {
 		/*
 		 * Nobody gets write access to an immutable file.
